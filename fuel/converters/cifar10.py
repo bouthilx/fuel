752cd236021aa6f43eb9efef9b2d31e7a296b1a9
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2015-08-28 14:43:52 -0400

    Changed pca to GPU

--- fuel/converters/cifar10.py
+++ fuel/converters/cifar10.py
@@ -213,7 +213,8 @@ def convert_cifar10_pca(directory, variance, output_file, patchsize=None):
         test_features -= train_features_mean_0
         test_features /= train_features_std_0
 
-    pca_backward, pca_forward, zca_backward, zca_forward = pca(train_features, variance, whiten=True)
+    pca_backward, pca_forward, zca_backward, zca_forward = pca(train_features,
+            variance, whiten=False)
     train_features_whitened = numpy.dot(train_features, pca_backward.T).astype("float32")
     if patchsize is None:
         test_features_whitened = numpy.dot(test_features, pca_backward.T).astype("float32")
@@ -307,7 +308,6 @@ def fill_subparser_pca_085(subparser):
     subparser.set_defaults(func=partial)
 
 
-
 def crop_patches_color(image, keypoints, patchsize):
     patches = numpy.zeros((len(keypoints), 3*patchsize**2))
     for i, k in enumerate(keypoints):
@@ -315,21 +315,80 @@ def crop_patches_color(image, keypoints, patchsize):
     return patches
 
 
-def pca(data, var_fraction, whiten=True):
-    """ principal components analysis of data (columnwise in array data), retaining as many components as required to retain var_fraction of the variance 
-    """
-    from numpy.linalg import eigh
-    u, v = eigh(numpy.cov(data, rowvar=0, bias=1))
-    v = v[:, numpy.argsort(u)[::-1]]
-    u.sort()
-    u = u[::-1]
-    u = u[u.cumsum()<=(u.sum()*var_fraction)]
-    numprincomps = u.shape[0]
-    u[u<SMALL] = SMALL
-    if whiten: 
-        backward_mapping = ((u**(-0.5))[:numprincomps][numpy.newaxis,:]*v[:,:numprincomps]).T
-        forward_mapping = (u**0.5)[:numprincomps][numpy.newaxis,:]*v[:,:numprincomps]
-    else: 
-        backward_mapping = v[:,:numprincomps].T
-        forward_mapping = v[:,:numprincomps]
-    return backward_mapping, forward_mapping, numpy.dot(v[:,:numprincomps], backward_mapping), numpy.dot(forward_mapping, v[:,:numprincomps].T)
+def pca(data, whiten=True, use_gpu = True, batchsize=100, contrast_norm=True, dataset_norm=True, verbose=True):
+    data = data.astype(np.float32)
+    ncases = data.shape[0]
+    nbatches = (ncases - 1) / batchsize + 1
+    # contrast normalization
+
+    if contrast_norm:
+        if verbose:
+            print 'using gpu'
+            print 'performing contrast normalization'
+        for bidx in range(nbatches):
+            start = bidx * batchsize
+            end = min((bidx + 1) * batchsize, ncases)
+            if use_gpu:
+                data[start:end] = theano_subtract_m1(data[start:end])
+                data[start:end] = theano_divide_s1(data[start:end])
+            else:
+                data[start:end] -= data[start:end].mean(1)[:, None]
+                s1 = data[start:end].std(1)[:, None]
+                data[start:end] /= s1 + s1.mean()
+
+    # normalization over dataset
+    m0=0
+    s0=1
+    if dataset_norm:
+        if verbose:
+            print 'performing normalization over dataset'
+        m0 = compute_mean0_batchwise(data, batchsize=batchsize, use_gpu=use_gpu, verbose=verbose)
+        for bidx in range(nbatches):
+            start = bidx * batchsize
+            end = min((bidx + 1) * batchsize, ncases)
+            if use_gpu:
+                data[start:end] = theano_subtract_row(data[start:end], m0)
+            else:
+                data[start:end] -= m0
+
+        s0 = compute_std0_batchwise(data, batchsize=batchsize, use_gpu=use_gpu, verbose=verbose)
+        s0 += s0.mean()
+        for bidx in range(nbatches):
+            start = bidx * batchsize
+            end = min((bidx + 1) * batchsize, ncases)
+            if use_gpu:
+                data[start:end] = theano_divide_row(data[start:end], s0)
+            else:
+                data[start:end] /= s0
+
+    if verbose:
+        print 'computing covariance matrix'
+    covmat = compute_covmat_batchwise(data, use_gpu=use_gpu, batchsize=batchsize, verbose=verbose)
+    if verbose:
+        print 'performing eigenvalue decomposition'
+    if whiten:
+        V, W, var_fracs = _get_pca_params_from_covmat(covmat, verbose=verbose)
+    else:
+        V, W, var_fracs = _get_pca_nowhite_params_from_covmat(covmat, verbose=verbose)
+
+    return V, W, m0, s0, var_fracs
+
+
+#def pca(data, var_fraction, whiten=True):
+#    """ principal components analysis of data (columnwise in array data), retaining as many components as required to retain var_fraction of the variance 
+#    """
+#    from numpy.linalg import eigh
+#    u, v = eigh(numpy.cov(data, rowvar=0, bias=1))
+#    v = v[:, numpy.argsort(u)[::-1]]
+#    u.sort()
+#    u = u[::-1]
+#    u = u[u.cumsum()<=(u.sum()*var_fraction)]
+#    numprincomps = u.shape[0]
+#    u[u<SMALL] = SMALL
+#    if whiten: 
+#        backward_mapping = ((u**(-0.5))[:numprincomps][numpy.newaxis,:]*v[:,:numprincomps]).T
+#        forward_mapping = (u**0.5)[:numprincomps][numpy.newaxis,:]*v[:,:numprincomps]
+#    else: 
+#        backward_mapping = v[:,:numprincomps].T
+#        forward_mapping = v[:,:numprincomps]
+#    return backward_mapping, forward_mapping, numpy.dot(v[:,:numprincomps], backward_mapping), numpy.dot(forward_mapping, v[:,:numprincomps].T)
